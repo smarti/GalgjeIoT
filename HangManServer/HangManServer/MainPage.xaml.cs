259c32735bfd60610bc09286540f063579649359
Marco Goessens <1622935goessens@zuyd.nl>
2018-01-02 16:05:50 +0100

    Verbinding

--- HangManServer/HangManServer/MainPage.xaml.cs
+++ HangManServer/HangManServer/MainPage.xaml.cs
@@ -12,7 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
+using Windows.Devices.Gpio;
+using System.Diagnostics;
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace HangManServer
@@ -22,9 +23,21 @@ namespace HangManServer
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private Instream server;
+
+        
+
         public MainPage()
         {
             this.InitializeComponent();
+            //Creer een nieuwe server die luistert naar poort 9000
+            //Poortnummer mag anders zijn, maar de clients moeten naar hetzelfde nummer luisteren
+            server = new Instream(9000);
+
+            //Koppel OnDataOntvangen aan de methode die uitgevoerd worden:
+            server.OnDataOntvangen += server.Server_OnDataOntvangen;
+            
         }
     }
 }
+
